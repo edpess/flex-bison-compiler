8dd5b6743250ae23459c9ecb1c8f99aa29a6a8f3
Alysson Oliveira <lssn.oliveira@gmail.com>
2018-12-28 12:02:01 -0300

    Adjusts to grammar and shell scripts permissions

--- build.sh
+++ build.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-bison -d -v -g matrix.y 
+bison -d -v -g matrix.y
 
 flex matrix.l
 
 cc lex.yy.c matrix.tab.c -o analizer -lfl -lm
 
 dot -T png -O matrix.dot
-xdg-open matrix.dot
-xdg-open matrix.dot.png
+#xdg-open matrix.dot
+#xdg-open matrix.dot.png
 
-echo
 echo "Use ./analizer to start the program"
-echo
 
 
 
